e6ddbf3e7bc1756a216694161ab1154462cc5c05
TheTimeLord32 <thomas.mandija@gmail.com>
2022-06-22 08:21:34 +0200

    Esame PW04

--- Vecchi Esami/PW04/script.js
+++ Vecchi Esami/PW04/script.js
@@ -1,5 +1,19 @@
 "use strict";
 
 window.onload = () => {
-	
+	fetchData();
+
 };
+
+function fetchData(){
+	fetch("data.json")
+	.then(response => response.json())
+	.then(json_obj => {
+		let canvas = document.querySelector(".main").appendChild(document.createElement("canvas"));
+		for(let i = 0; i < json_obj.length; i++){
+			let ctx = canvas.getContext("2d");
+			ctx.fillStyle = json_obj[i].colore;
+			ctx.fillRect(0, 0, json_obj[i].pos_orizzontale, json_obj[i].pos_verticale);
+		}
+	});
+}
