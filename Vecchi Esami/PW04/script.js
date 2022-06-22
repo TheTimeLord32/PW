1efb8964ba045579e6cd66276bd194d2034ac88d
TheTimeLord32 <thomas.mandija@gmail.com>
2022-06-22 19:20:38 +0200

    Esame PW04

--- Vecchi Esami/PW04/script.js
+++ Vecchi Esami/PW04/script.js
@@ -1,19 +1,35 @@
 "use strict";
 
 window.onload = () => {
-	fetchData();
-
+	let titolo = document.querySelector("header h1");
+	titolo.addEventListener("click", fetchData);	
 };
 
 function fetchData(){
 	fetch("data.json")
 	.then(response => response.json())
 	.then(json_obj => {
-		let canvas = document.querySelector(".main").appendChild(document.createElement("canvas"));
-		for(let i = 0; i < json_obj.length; i++){
-			let ctx = canvas.getContext("2d");
-			ctx.fillStyle = json_obj[i].colore;
-			ctx.fillRect(0, 0, json_obj[i].pos_orizzontale, json_obj[i].pos_verticale);
+		let array = json_obj;
+		let main = document.querySelector("main");
+		
+		for(let i = 0; i < array.length; i++){
+			let div = main.appendChild(document.createElement("div"));
+			let p = div.appendChild(document.createElement("p"));
+			
+			console.log("Titolo premuto");
+			console.log("Data ", i, ": ", array[i]);
+				
+			p.style.backgroundColor = array[i].colore;
+			p.style.width = "10vh";
+			p.style.height = "10vh";
+			p.style.position = "relative";
+			p.style.left = array[i].pos_x + "vh";
+			p.style.top = array[i].pos_y + "vh";
+
+			div.addEventListener("click", () => {
+				main.removeChild(div);
+				console.log("Div eliminato");
+			});
 		}
 	});
 }
