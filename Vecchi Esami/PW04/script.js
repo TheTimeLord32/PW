d20640b60651aec8980e8421abae11a15102d516
TheTimeLord32 <thomas.mandija@gmail.com>
2022-06-25 12:12:53 +0200

    Await added

--- Vecchi Esami/PW04/script.js
+++ Vecchi Esami/PW04/script.js
@@ -2,7 +2,7 @@
 
 window.onload = () => {
 	let titolo = document.querySelector("header h1");
-	titolo.addEventListener("click", fetchData);	
+	titolo.addEventListener("click", fetchData2);	
 };
 
 function fetchData(){
@@ -35,4 +35,35 @@ function fetchData(){
 		alert("Errore: " + err);
 		console.log("Errore: " + err);
 	});
+}
+
+async function fetchData2(){
+	try {
+		let data = await fetch("data.json");
+		let json_obj = await data.json();
+		let array = json_obj;
+		let main = document.querySelector("main");
+		
+		for(let i = 0; i < array.length; i++){
+			let div = main.appendChild(document.createElement("div"));
+				
+			console.log("Titolo premuto");
+			console.log("Data ", i, ": ", array[i]);
+				
+			div.style.backgroundColor = array[i].colore;
+			div.style.width = "10vh";
+			div.style.height = "10vh";
+			div.style.position = "absolute";
+			div.style.left = array[i].pos_x + "%";
+			div.style.top = array[i].pos_y + "%";
+
+			div.addEventListener("click", () => {
+				main.removeChild(div);
+				console.log("Div eliminato");
+			});
+		}
+	} catch (error) {
+		alert("Errore: " + error);
+		console.log("Errore: " + error);
+	}
 }
